af6bbc72750b6ade30ebd855d6611898ea324e63
RomanPR <prikhodjko.roman@gmail.com>
2026-07-12 07:01:01 +0300

    Visual update & Deploy update

--- src/app/common-ui/directives/auto-resize.directive.ts
+++ src/app/common-ui/directives/auto-resize.directive.ts
@@ -4,18 +4,21 @@ import { Directive, ElementRef, inject, input } from '@angular/core';
     selector: '[appAutoResize]',
     standalone: true,
     host: {
-        '(input)': 'onInput($event)'
+        '(input)': 'onInput()'
     }
 })
 export class AutoResizeDirective {
     private hostElement = inject<ElementRef<HTMLTextAreaElement>>(ElementRef);
     maxHeight = input<number>(300);
 
-    onInput(event: Event) {
+    ngOnInit() {
+        this.hostElement.nativeElement.style.resize = 'none';
+    }
+
+    onInput() {
         const textarea = this.hostElement.nativeElement;
 
         textarea.style.height = 'auto';
-        textarea.style.resize = 'none';
 
         if (textarea.scrollHeight > this.maxHeight()) {
             textarea.style.overflowY = 'scroll';
