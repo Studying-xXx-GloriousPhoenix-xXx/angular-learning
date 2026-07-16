fe2d5eddfe3e5c33e50325aa9786c4f994ded5f0
RomanPR <prikhodjko.roman@gmail.com>
2026-07-16 19:47:23 +0300

    Directive Decoration

--- src/app/common-ui/directives/auto-resize.directive.ts
+++ src/app/common-ui/directives/auto-resize.directive.ts
@@ -9,24 +9,31 @@ import { Directive, ElementRef, inject, input } from '@angular/core';
 })
 export class AutoResizeDirective {
     private hostElement = inject<ElementRef<HTMLTextAreaElement>>(ElementRef);
+    textarea = this.hostElement.nativeElement;
     maxHeight = input<number>(300);
 
     ngOnInit() {
-        this.hostElement.nativeElement.style.resize = 'none';
+        this.textarea.style.resize = 'none';
+        this.textarea.style.height = 'auto';
+        this.onInput();
     }
 
     onInput() {
-        const textarea = this.hostElement.nativeElement;
-
-        textarea.style.height = 'auto';
-
-        if (textarea.scrollHeight > this.maxHeight()) {
-            textarea.style.overflowY = 'scroll';
-            textarea.style.height = this.maxHeight() + 'px';
+        const style = getComputedStyle(this.textarea);
+        const borderHeight = 
+            parseFloat(style.borderTopWidth) + parseFloat(style.borderBottomWidth);
+    
+        this.textarea.style.height = 'auto';
+    
+        const newHeight = this.textarea.scrollHeight + borderHeight;
+    
+        if (newHeight > this.maxHeight()) {
+            this.textarea.style.overflowY = 'scroll';
+            this.textarea.style.height = this.maxHeight() + 'px';
         }
         else {
-            textarea.style.overflowY = 'hidden';
-            textarea.style.height = textarea.scrollHeight + 'px';
+            this.textarea.style.overflowY = 'hidden';
+            this.textarea.style.height = newHeight + 'px';
         }
     }
 }
